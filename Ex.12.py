72cadb9463c7430abc71312b70756893564a0ee1
funrk <167949789+funrk@users.noreply.github.com>
2024-05-07 22:28:46 +0530

    Add files via upload

---
+++ Ex.12.py
@@ -0,0 +1,13 @@
+void setup () {
+put your setup code here, to run once:
+Serial.begin(9600);
+Serial.println();
+
+float x = 10.0;
+
+Serial.println(sq(x));
+
+//
+
+void loop () {
+put your main code here, to run repeatedly:
